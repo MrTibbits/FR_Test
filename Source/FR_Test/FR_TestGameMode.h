70e68afc6cc776b6aa6d5fd9abfb6ad4d232ccd2
suhaib rehman <diezalb0y@gmail.com>
2022-05-07 13:03:24 +0300

    adding character and game mode header file

---
+++ Source/FR_Test/FR_TestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FR_TestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFR_TestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFR_TestGameMode();
+};
+
+
+
